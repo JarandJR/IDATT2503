2739f8d7b56eecc7fcd1b832668e66be659af3e5
Jarand Jensen Romestrand <jarandjr@stud.ntnu.no>
2023-10-03 19:37:43 +0200

    Testing CI

--- oving5/tests/format_string_test.c
+++ oving5/tests/format_string_test.c
@@ -2,41 +2,41 @@
 #include <string.h>
 #include <stdlib.h>
 
-extern char *format(char *input);
+extern char *format_string(char *in);
 
 int main() {
     char *result;
 
-    result = format("hello");
+    result = format_string("hello");
     assert(strcmp(result, "hello") == 0);
     free(result);
 
-    result = format("hello&world");
+    result = format_string("hello&world");
     assert(strcmp(result, "hello&amp;world") == 0);
     free(result);
 
-    result = format("hello<world");
+    result = format_string("hello<world");
     assert(strcmp(result, "hello&lt;world") == 0);
     free(result);
 
-    result = format("hello>world");
+    result = format_string("hello>world");
     assert(strcmp(result, "hello&gt;world") == 0);
     free(result);
 
-    result = format("This is a <test> & example.");
+    result = format_string("This is a <test> & example.");
     assert(strcmp(result, "This is a &lt;test&gt; &amp; example.") == 0);
     free(result);
 
 
-    result = format("hello&");
+    result = format_string("hello&");
     assert(strcmp(result, "hello&gt;") != 0);
     free(result);
 
-    result = format("hello<");
+    result = format_string("hello<");
     assert(strcmp(result, "hello&gt;") != 0);
     free(result);
 
-    result = format("hello>");
+    result = format_string("hello>");
     assert(strcmp(result, "hello&lt;") != 0);
     free(result);
 
--- oving5/tests/fuzz_test.c
+++ oving5/tests/fuzz_test.c
@@ -2,14 +2,14 @@
 #include <string.h>
 #include <stdlib.h>
 
-extern char *format(char *input);
+extern char *format_string(char *in);
 
 int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size) {
     char *str = (char *)malloc(sizeof(char) * size + 1);
     memcpy(str, data, size);
     str[size] = '\0';
 
-    /*char *result =*/ format(str); 
+    /*char *result =*/ format_string(str);
     /*if (result != NULL) {
         free(result);
     }*/
