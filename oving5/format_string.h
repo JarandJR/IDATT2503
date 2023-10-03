a6efeb2cf8b100b8dfc7a025a5f3ef3356647138
Jarand Jensen Romestrand <jarandjr@stud.ntnu.no>
2023-10-03 19:35:04 +0200

    Testing CI

--- oving5/format_string.h
+++ oving5/format_string.h
@@ -1,32 +1,6 @@
-#include <string.h>
-#include <stdlib.h>
+#ifndef FORMAT_H
+#define FORMAT_H
 
-char* format(char *in) {
-    size_t l = strlen(in);
-    size_t len = l;
-    for (size_t i = 0; i < l; ++i) {
-        if (in[i] == '&') len += 4;
-        else if (in[i] == '<' || in[i] == '>') len += 3;
-    }
-    char *out = malloc(len + 1);
-    if (out == NULL) return NULL;
+char* format(char *in);
 
-    size_t pos = 0;
-    for (size_t i = 0; i < strlen(in); i++) {
-        if (in[i] == '&') {
-            strcpy(out + pos, "&amp;");
-            pos += 5;
-        } else if (in[i] == '<') {
-            strcpy(out + pos, "&lt;");
-            pos += 4;
-        } else if(in[i] == '>') {
-            strcpy(out + pos, "&gt;");
-            pos += 4;
-        } else {
-            out[pos++]=in[i];
-        }
-    }
-
-    out[pos] = '\0';
-    return out;
-}
+#endif
